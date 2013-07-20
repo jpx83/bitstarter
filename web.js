5ac00c7ddfe060c4d7ec7b8b959591f465200983
Giampiero <giampieromiccoli@libero.it>
2013-07-20 23:44:29 +0200

    Update web.js

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var buff = fs.readFileSync("index.html");
-var content = buff.toString()
+var content = buff.toString();
 
 app.get('/', function(request, response) {
-  response.send(content');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
