310674301dff83b00052bc007514dcc1e140f200
Giampiero <giampieromiccoli@libero.it>
2013-07-20 23:40:46 +0200

    Update web.js

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buff = fs.readFileSync("index.html");
+var content = buff.toString()
+
 app.get('/', function(request, response) {
-  response.send('Hello World from index.html');
+  response.send(content');
 });
 
 var port = process.env.PORT || 5000;
