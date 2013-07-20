e98fbb448ae79c9e1bb98af870c221049e7b4c2f
Giampiero <giampieromiccoli@libero.it>
2013-07-21 00:01:39 +0200

    Update web.js

--- web.js
+++ web.js
@@ -2,14 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//var str = fs.readFileSync('index.html');
-//var buffer = new Buffer(str.length);
-//for (var i = 0; i < str.length ; i++) {
-//  buf[i] = str.charCodeAt(i);
-//}
+var fs = require('fs');
+var str = fs.readFileSync('index.html');
+var buffer = new Buffer(str.length);
+for (var i = 0; i < str.length ; i++) {
+  buf[i] = str.charCodeAt(i);
+}
 
 app.get('/', function(request, response) {
-  response.send('buf');
+  response.send(buf);
 });
 
 var port = process.env.PORT || 5000;
