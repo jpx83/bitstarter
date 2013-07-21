299e41788f97e8cdf6730e90a3aeea206f661bad
Giampiero <giampieromiccoli@libero.it>
2013-07-21 09:18:45 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+/*
 var fs = require('fs');
 var express = require('express');
 
@@ -26,3 +27,16 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
+
+var http = require('http');
+var fs = require('fs');
+var sys = require('sys');
+
+http.createServer(function(req, res){
+    fs.readFile('index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+}).listen(5000);
